4ebef07452fb6e3f0f9bf5f6f0490812855996fd
hadassa <hadassasantiago300@gmail.com>
2024-08-20 17:08:00 -0300

    .

---
+++ cmquestion.js
@@ -0,0 +1,110 @@
+et questions = [
+    {
+        question: "Qual planta marinha é conhecida por suas folhas longas e finas que formam vastos prados subaquáticos?",
+        answers: [
+            {option:"Algas vermelhas", correct: false },
+            {option:"Coral cérebro", correct: false },
+            {option:"Ervas marinhas", correct: true },
+            {option:"Anêmonas-do-mar", correct: false },
+            {option:"Lírios-d'água", correct: false },
+        ],
+    },
+    {
+        question: "Qual é o nome dado às grandes algas que crescem em águas frias e são frequentemente chamadas de 'kelp florestas'?"
+        answers: [
+            {option:"Algas corais", correct: false },
+            {option:"Plâncton", correct: false },
+            {option:"Salgas", correct: false },
+            {option:"Algas vermelhas", correct: false },
+            {option:"Algas pardas", correct: true },
+        ],
+    },
+    
+    {
+        question: "Qual dessas algas é geralmente encontrada em águas tropicais e tem uma cor vermelho-rosada?",
+        answers: [
+            {option:"Algas verdes", correct: false },
+            {option:"Algas marrons", correct: false },
+            {option:"Algas vermelhas ", correct: true },
+            {option:"Algas azuis-verdes", correct: false },
+            {option:"Algas douradas", correct: false },
+        ],
+    },
+    
+    {
+        question: "Qual tipo de planta marinha é conhecida por seu papel na formação de recifes de corais?",
+        answers: [
+            {option:"Algas marrons", correct: false },
+            {option:"Lírios-do-mar", correct: false },
+            {option:"Corais", correct: true },
+            {option:"Plâncton", correct: false },
+            {option:"Sargasso", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual é a função principal das algas verdes nos ecossistemas marinhos?",
+        answers: [
+            {option:"Formar recifes de corais", correct: false },
+            {option:" Produzir oxigênio através da fotossíntese", correct: true },
+            {option:"Produzir o recife de barro", correct: false },
+            {option:"Servir como abrigo para peixes pelágicos", correct: false },
+            {option:"Absorver poluentes químicos da água", correct: false },
+        ],
+    },
+
+    {
+        question: "",
+        answers: [
+            {option:" ", correct: true },
+            {option:"", correct: false },
+            {option:"", correct: false },
+            {option:"", correct: false },
+            {option:"", correct: false },
+        ],
+    },
+
+    {
+        question: "",
+        answers: [
+            {option:"", correct: false },
+            {option:"", correct: false },
+            {option:"", correct: true },
+            {option:"", correct: false },
+            {option:"", correct: false },
+        ],
+    },
+
+    {
+        question: "",
+        answers: [
+            {option:"", correct: false },
+            {option:"", correct: true },
+            {option:" ", correct: false },
+            {option:"", correct: false },
+            {option:"", correct: false },
+        ],
+    },
+
+    {
+        question: "",
+        answers: [
+            {option:"", correct: true },
+            {option:"", correct: false },
+            {option:" ", correct: false },
+            {option:" ", correct: false },
+            {option:" ", correct: false },
+        ],
+    },
+
+    {
+        question: "",
+        answers: [
+            {option:"", correct: false },
+            {option:"", correct: false },
+            {option:"", correct: false },
+            {option:" ", correct: true },
+            {option:"", correct: false },
+        ],
+    },
+]
